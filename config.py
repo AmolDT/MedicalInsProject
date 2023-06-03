0ef17e3c7b7ce81f2d8f145ebf7b8051e4ebb230
AmolDT <135360721+AmolDT@users.noreply.github.com>
2023-06-03 13:07:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 
 
-MODEL_FILE_PATH=r"D:\Data Science Class\ML Models\Linear Regression\e2e Regression Models\Lin.Reg. e2e Models\28_Amol_Tekale_Medical_insurance_charges_prediction_model\project_app\Linear_model.pkl"
+MODEL_FILE_PATH=r"\project_app\Linear_model.pkl"
 
-JSON_FILE_PATH=r"D:\Data Science Class\ML Models\Linear Regression\e2e Regression Models\Lin.Reg. e2e Models\28_Amol_Tekale_Medical_insurance_charges_prediction_model\project_app\json_data.json"
+JSON_FILE_PATH=r"\project_app\json_data.json"
